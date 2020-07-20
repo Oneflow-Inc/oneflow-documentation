ab4be4118bb28517717d6957d251469b1ead9592
Shenghang Tsai <jackalcooper@gmail.com>
2020-07-20 10:38:46 +0800

    New translations synchronize_batch_job.py (English)

---
+++ en/docs/code/basics_topics/synchronize_batch_job.py
@@ -0,0 +1,63 @@
+import numpy as np
+import oneflow as flow
+from mnist_util import load_data
+
+BATCH_SIZE = 100
+
+
+def lenet(data, train=False):
+    initializer = flow.truncated_normal(0.1)
+    conv1 = flow.layers.conv2d(data, 32, 5, padding='SAME', activation=flow.nn.relu,
+                               kernel_initializer=initializer, name="conv1")
+    pool1 = flow.nn.max_pool2d(conv1, ksize=2, strides=2, padding='SAME', name="pool1")
+    conv2 = flow.layers.conv2d(pool1, 64, 5, padding='SAME', activation=flow.nn.relu,
+                               kernel_initializer=initializer, name="conv2")
+    pool2 = flow.nn.max_pool2d(conv2, ksize=2, strides=2, padding='SAME', name="pool2")
+    reshape = flow.reshape(pool2, [pool2.shape[0], -1])
+    hidden = flow.layers.dense(reshape, 512, activation=flow.nn.relu, kernel_initializer=initializer, name="hidden")
+    if train: hidden = flow.nn.dropout(hidden, rate=0.5)
+    return flow.layers.dense(hidden, 10, kernel_initializer=initializer, name="outlayer")
+
+
+def get_eval_config():
+    config = flow.function_config()
+    config.default_data_type(flow.float)
+    return config
+
+
+@flow.global_function(get_eval_config())
+def eval_job(images=flow.FixedTensorDef((BATCH_SIZE, 1, 28, 28), dtype=flow.float),
+             labels=flow.FixedTensorDef((BATCH_SIZE,), dtype=flow.int32)):
+    with flow.scope.placement("gpu", "0:0"):
+        logits = lenet(images, train=True)
+        loss = flow.nn.sparse_softmax_cross_entropy_with_logits(labels, logits, name="softmax_loss")
+
+    return [labels, logits]
+
+
+g_total = 0
+g_correct = 0
+
+
+def acc(labels, logits):
+    global g_total
+    global g_correct
+
+    predictions = np.argmax(logits.ndarray(), 1)
+    right_count = np.sum(predictions == labels)
+    g_total += labels.shape[0]
+    g_correct += right_count
+
+
+if __name__ == '__main__':
+
+    check_point = flow.train.CheckPoint()
+    check_point.load("./lenet_models_1")
+
+    (train_images, train_labels), (test_images, test_labels) = load_data(BATCH_SIZE, BATCH_SIZE)
+    for epoch in range(1):
+        for i, (images, labels) in enumerate(zip(train_images, train_labels)):
+            labels, logits = eval_job(images, labels).get()
+            acc(labels, logits)
+
+    print("accuracy: {0:.1f}%".format(g_correct * 100 / g_total))
