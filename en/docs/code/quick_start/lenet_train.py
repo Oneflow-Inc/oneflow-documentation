a25983c59e3972fc08f06cae18a13ea2c8686e94
Shenghang Tsai <jackalcooper@gmail.com>
2020-07-20 13:32:04 +0800

    New translations lenet_train.py (English)

---
+++ en/docs/code/quick_start/lenet_train.py
@@ -0,0 +1,54 @@
+#lenet_train.py
+import numpy as np
+import oneflow as flow
+from mnist_util import load_data
+from PIL import Image
+BATCH_SIZE = 100
+
+
+def lenet(data, train=False):
+    initializer = flow.truncated_normal(0.1)
+    conv1 = flow.layers.conv2d(data, 32, 5, padding='SAME', activation=flow.nn.relu, name='conv1',
+                               kernel_initializer=initializer)
+    pool1 = flow.nn.max_pool2d(conv1, ksize=2, strides=2, padding='SAME', name='pool1')
+    conv2 = flow.layers.conv2d(pool1, 64, 5, padding='SAME', activation=flow.nn.relu, name='conv2',
+                               kernel_initializer=initializer)
+    pool2 = flow.nn.max_pool2d(conv2, ksize=2, strides=2, padding='SAME', name='pool2', )
+    reshape = flow.reshape(pool2, [pool2.shape[0], -1])
+    hidden = flow.layers.dense(reshape, 512, activation=flow.nn.relu, kernel_initializer=initializer, name='dense1')
+    if train: hidden = flow.nn.dropout(hidden, rate=0.5, name="dropout")
+    return flow.layers.dense(hidden, 10, kernel_initializer=initializer, name='dense2')
+
+
+def get_train_config():
+    config = flow.function_config()
+    config.default_data_type(flow.float)
+    config.train.primary_lr(0.1)
+    config.train.model_update_conf({"naive_conf": {}})
+    return config
+
+
+@flow.global_function(get_train_config())
+def train_job(images=flow.FixedTensorDef((BATCH_SIZE, 1, 28, 28), dtype=flow.float),
+              labels=flow.FixedTensorDef((BATCH_SIZE,), dtype=flow.int32)):
+    with flow.scope.placement("gpu", "0:0"):
+        logits = lenet(images, train=True)
+        loss = flow.nn.sparse_softmax_cross_entropy_with_logits(labels, logits, name="softmax_loss")
+    flow.losses.add_loss(loss)
+    return loss
+
+if __name__ == '__main__':
+    flow.config.gpu_device_num(1)
+    check_point = flow.train.CheckPoint()
+    check_point.init()
+
+    (train_images, train_labels), (test_images, test_labels) = load_data(BATCH_SIZE)
+
+    for epoch in range(50):
+        for i, (images, labels) in enumerate(zip(train_images, train_labels)):
+            loss = train_job(images, labels).get().mean()
+            if i % 20 == 0: print(loss)
+            if loss < 0.01:
+                break
+    check_point.save('./lenet_models_1')  # need remove the existed folder
+    print("model saved")
