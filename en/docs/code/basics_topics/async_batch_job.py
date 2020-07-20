84304062e2a3574fea4354a4ddb33e99e124879a
Shenghang Tsai <jackalcooper@gmail.com>
2020-07-20 19:17:04 +0800

    New translations async_batch_job.py (English)

--- en/docs/code/basics_topics/async_batch_job.py
+++ en/docs/code/basics_topics/async_batch_job.py
@@ -21,41 +21,41 @@ def get_eval_config():
 @flow.global_function(get_eval_config())
 def eval_job(images=flow.FixedTensorDef((BATCH_SIZE, 1, 28, 28), dtype=flow.float),
              labels=flow.FixedTensorDef((BATCH_SIZE,), dtype=flow.int32)):
-    with flow.scope.placement("cpu", "0:0"):
+    main_eval()
+        with flow.scope.placement("cpu", "0:0"):
         logits = mlp(images)
-        loss = flow.nn.sparse_softmax_cross_entropy_with_logits(labels, logits, name="softmax_loss")
 
-    return {"labels": labels, "logits": logits}
+    loss = flow.nn.sparse_softmax_cross_entropy_with_logits(labels, logits, name="softmax_loss")
 
 
+return {"labels": labels, "logits": logits}
 g_total = 0
+
+
 g_correct = 0
-
-
-def acc(eval_result):
+    def acc(eval_result):
     global g_total
+
     global g_correct
-
     labels = eval_result["labels"]
-    logits = eval_result["logits"]
 
+    logits = eval_result["logits"]
     predictions = np.argmax(logits.ndarray(), 1)
     right_count = np.sum(predictions == labels)
     g_total += labels.shape[0]
-    g_correct += right_count
 
 
-def main_eval():
+g_correct += right_count
+    def main_eval():
     # flow.config.enable_debug_mode(True)
     check_point = flow.train.CheckPoint()
     check_point.load('./mlp_models_1')
     (train_images, train_labels), (test_images, test_labels) = load_data(BATCH_SIZE)
-    for epoch in range(1):
-        for i, (images, labels) in enumerate(zip(test_images, test_labels)):
-            eval_job(images, labels).async_get(acc)
+        for epoch in range(1):
+            for i, (images, labels) in enumerate(zip(test_images, test_labels)):
 
-    print("accuracy: {0:.1f}%".format(g_correct * 100 / g_total))
+    eval_job(images, labels).async_get(acc)
 
 
-if __name__ == '__main__':
-    main_eval()
+print("accuracy: {0:.1f}%".format(g_correct * 100 / g_total))
+    if __name__ == '__main__':
