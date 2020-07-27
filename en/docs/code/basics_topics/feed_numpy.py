11c9697e11b4ea9fb71f281685ea0c12d73fc417
Shenghang Tsai <jackalcooper@gmail.com>
2020-07-28 03:47:21 +0800

    New translations feed_numpy.py (English)

--- en/docs/code/basics_topics/feed_numpy.py
+++ en/docs/code/basics_topics/feed_numpy.py
@@ -2,16 +2,17 @@
 import numpy as np
 import oneflow as flow
 import oneflow.typing as oft
+from typing import Tuple
 
 @flow.global_function(flow.function_config())
 def test_job(images:oft.Numpy.Placeholder((32, 1, 28, 28), dtype=flow.float),
-             labels:oft.Numpy.Placeholder((32,), dtype=flow.int32)):
+             labels:oft.Numpy.Placeholder((32,), dtype=flow.int32)) -> Tuple[oft.Numpy, oft.Numpy]:
     # do something with images or labels
-    return images, labels
+    return (images, labels)
 
 
 if __name__ == '__main__':
     images_in = np.random.uniform(-10, 10, (32, 1, 28, 28)).astype(np.float32)
     labels_in = np.random.randint(-10, 10, (32,)).astype(np.int32)
-    images, labels = test_job(images_in, labels_in).get()
+    images, labels = test_job(images_in, labels_in)
     print(images.shape, labels.shape)
