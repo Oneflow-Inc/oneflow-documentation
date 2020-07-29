af2d149b8112489017e0db7cd7207d1ba7158c09
Yao Chi <yaochi@oneflow.org>
2020-07-29 11:42:51 +0800

    fix watch_diff article

--- cn/docs/code/extended_topics/test_watch.py
+++ cn/docs/code/extended_topics/test_watch.py
@@ -1,16 +1,17 @@
+#test_watch.py
 import numpy as np
 import oneflow as flow
 import oneflow.typing as oft
 
-def cb(y):
-    print("out", y.ndarray())
+def watch_handler(y:oft.Numpy):
+    print("out:", y)
 
 @flow.global_function()
-def ReluJob(x:oft.Numpy.Placeholder((10,))):
+def ReluJob(x:oft.Numpy.Placeholder((5,))) -> None:
     y = flow.nn.relu(x)
-    flow.watch(y, cb)
+    flow.watch(y, watch_handler)
 
 flow.config.gpu_device_num(1)
-data = np.random.uniform(-1, 1, 10).astype(np.float32)
-print("in: ", data)
+data = np.random.uniform(-1, 1, 5).astype(np.float32)
+print("in:", data)
 ReluJob(data)
