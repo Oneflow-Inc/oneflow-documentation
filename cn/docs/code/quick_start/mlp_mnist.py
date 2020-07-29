137367e671e5b4100c3bd559111e1ba590726e21
Flowingsun007 <flowingsun007@163.com>
2020-07-29 10:56:01 +0800

    replace function_config with new api

--- cn/docs/code/quick_start/mlp_mnist.py
+++ cn/docs/code/quick_start/mlp_mnist.py
@@ -1,28 +1,23 @@
 # mlp_mnist.py
 import oneflow as flow
-import oneflow.typing as oft
+import oneflow.typing as tp
 
 BATCH_SIZE = 100
 
 
-def get_train_config():
-    config = flow.function_config()
-    config.default_data_type(flow.float)
-    config.train.primary_lr(0.1)
-    config.train.model_update_conf({"naive_conf": {}})
-    return config
-
-
-@flow.global_function(get_train_config())
-def train_job(images:oft.Numpy.Placeholder((BATCH_SIZE, 1, 28, 28), dtype=flow.float),
-              labels:oft.Numpy.Placeholder((BATCH_SIZE,), dtype=flow.int32)) -> oft.Numpy:
+@flow.global_function(type="train")
+def train_job(images:tp.Numpy.Placeholder((BATCH_SIZE, 1, 28, 28), dtype=flow.float),
+              labels:tp.Numpy.Placeholder((BATCH_SIZE,), dtype=flow.int32)) -> tp.Numpy:
     with flow.scope.placement("cpu", "0:0"):
         initializer = flow.truncated_normal(0.1)
         reshape = flow.reshape(images, [images.shape[0], -1])
-        hidden = flow.layers.dense(reshape, 512, activation=flow.nn.relu, kernel_initializer=initializer)
-        logits = flow.layers.dense(hidden, 10, kernel_initializer=initializer)
+        hidden = flow.layers.dense(reshape, 512, activation=flow.nn.relu, kernel_initializer=initializer, name="dense1")
+        logits = flow.layers.dense(hidden, 10, kernel_initializer=initializer, name="dense2")
         loss = flow.nn.sparse_softmax_cross_entropy_with_logits(labels, logits)
-    flow.losses.add_loss(loss)
+
+    # flow.losses.add_loss(loss)
+    lr_scheduler = flow.optimizer.PiecewiseConstantScheduler([], [0.1])
+    flow.optimizer.SGD(lr_scheduler, momentum=0).minimize(loss)
 
     return loss
 
