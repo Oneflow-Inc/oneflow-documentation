af3fb41e3ae349b1ad365f72ecdc0fd9c199fd07
Flowingsun007 <flowingsun007@163.com>
2020-07-29 11:28:50 +0800

    async_single_job.py >> replace function_config with new api

--- cn/docs/code/basics_topics/async_single_job.py
+++ cn/docs/code/basics_topics/async_single_job.py
@@ -1,20 +1,12 @@
 import oneflow as flow
-import oneflow.typing as oft
+import oneflow.typing as tp
 
 BATCH_SIZE = 100
 
 
-def get_train_config():
-    config = flow.function_config()
-    config.default_data_type(flow.float)
-    config.train.primary_lr(0.1)
-    config.train.model_update_conf({"naive_conf": {}})
-    return config
-
-
-@flow.global_function(get_train_config())
-def train_job(images:oft.Numpy.Placeholder((BATCH_SIZE, 1, 28, 28), dtype=flow.float),
-              labels:oft.Numpy.Placeholder((BATCH_SIZE,), dtype=flow.int32)) -> oft.Callback[oft.Numpy]:
+@flow.global_function(type="train")
+def train_job(images: tp.Numpy.Placeholder((BATCH_SIZE, 1, 28, 28), dtype=flow.float),
+              labels: tp.Numpy.Placeholder((BATCH_SIZE,), dtype=flow.int32)) -> tp.Callback[tp.Numpy]:
     # mlp
     initializer = flow.truncated_normal(0.1)
     reshape = flow.reshape(images, [images.shape[0], -1])
@@ -22,19 +14,20 @@ def train_job(images:oft.Numpy.Placeholder((BATCH_SIZE, 1, 28, 28), dtype=flow.f
     logits = flow.layers.dense(hidden, 10, kernel_initializer=initializer, name="output")
 
     loss = flow.nn.sparse_softmax_cross_entropy_with_logits(labels, logits, name="softmax_loss")
-    flow.losses.add_loss(loss)
+    lr_scheduler = flow.optimizer.PiecewiseConstantScheduler([], [0.1])
+    flow.optimizer.SGD(lr_scheduler, momentum=0).minimize(loss)
     return loss
 
 
 g_i = 0
-def cb_print_loss(result:oft.Numpy):
+def cb_print_loss(result: tp.Numpy):
     global g_i
     if g_i % 20 == 0:
         print(result.mean())
     g_i += 1
 
 
-def main_train():
+def main():
     check_point = flow.train.CheckPoint()
     check_point.init()
 
@@ -47,4 +40,4 @@ def main_train():
 
 
 if __name__ == '__main__':
-    main_train()
+    main()
