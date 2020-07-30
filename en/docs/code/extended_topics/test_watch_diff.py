fecb8fb48a3cba527e98d9af83b79b8de6f79377
Shenghang Tsai <jackalcooper@gmail.com>
2020-07-30 10:59:20 +0800

    New translations test_watch_diff.py (English)

--- en/docs/code/extended_topics/test_watch_diff.py
+++ en/docs/code/extended_topics/test_watch_diff.py
@@ -1,10 +1,10 @@
 # test_watch_diff.py
 import oneflow as flow
-import oneflow.typing as oft
+import oneflow.typing as tp
 
 BATCH_SIZE = 100
 
-def watch_diff_handler(blob: oft.Numpy):
+def watch_diff_handler(blob: tp.Numpy):
     print("watch_diff_handler:", blob, blob.shape, blob.dtype)
 
 def get_train_config():
@@ -14,8 +14,8 @@ def get_train_config():
 
 
 @flow.global_function(type="train", function_config=get_train_config())
-def train_job(images:oft.Numpy.Placeholder((BATCH_SIZE, 1, 28, 28), dtype=flow.float),
-              labels:oft.Numpy.Placeholder((BATCH_SIZE,), dtype=flow.int32)) -> oft.Numpy:
+def train_job(images:tp.Numpy.Placeholder((BATCH_SIZE, 1, 28, 28), dtype=flow.float),
+              labels:tp.Numpy.Placeholder((BATCH_SIZE,), dtype=flow.int32)) -> tp.Numpy:
     with flow.scope.placement("cpu", "0:0"):
         initializer = flow.truncated_normal(0.1)
         reshape = flow.reshape(images, [images.shape[0], -1])
