0606445a2aaf3d1a9a4bb1f57ed0e86d8aedf59d
Shenghang Tsai <jackalcooper@gmail.com>
2020-07-30 09:05:20 +0800

    New translations eager_mlp_mnist.py (English)

---
+++ en/docs/code/extended_topics/eager_mlp_mnist.py
@@ -0,0 +1,33 @@
+# eager_mlp_mnist.py
+import oneflow as flow
+import oneflow.typing as tp
+
+BATCH_SIZE = 100
+
+
+@flow.global_function(type="train")
+def train_job(images:tp.Numpy.Placeholder((BATCH_SIZE, 1, 28, 28), dtype=flow.float),
+              labels:tp.Numpy.Placeholder((BATCH_SIZE,), dtype=flow.int32)) -> tp.Numpy:
+    with flow.scope.placement("cpu", "0:0"):
+        initializer = flow.truncated_normal(0.1)
+        reshape = flow.reshape(images, [images.shape[0], -1])
+        hidden = flow.layers.dense(reshape, 512, activation=flow.nn.relu, kernel_initializer=initializer, name="dense1")
+        logits = flow.layers.dense(hidden, 10, kernel_initializer=initializer, name="dense2")
+        loss = flow.nn.sparse_softmax_cross_entropy_with_logits(labels, logits)
+
+    lr_scheduler = flow.optimizer.PiecewiseConstantScheduler([], [0.1])
+    flow.optimizer.SGD(lr_scheduler, momentum=0).minimize(loss)
+
+    return loss
+
+
+if __name__ == '__main__':
+    flow.enable_eager_execution(True)
+    check_point = flow.train.CheckPoint()
+    check_point.init()
+
+    (train_images, train_labels), (test_images, test_labels) = flow.data.load_mnist(BATCH_SIZE, BATCH_SIZE)
+    for i, (images, labels) in enumerate(zip(train_images, train_labels)):
+        loss = train_job(images, labels)
+        if i % 20 == 0:
+            print(loss.mean())
