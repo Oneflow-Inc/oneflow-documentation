0f954a61ff246938430a7c92cc360b3f7e9fb62e
MARD1NO <359521840@qq.com>
2020-08-05 14:36:27 +0800

    fix link in en docs

---
+++ en/docs/code/extended_topics/hybrid_parallelism_mlp.py
@@ -0,0 +1,52 @@
+from mnist_util import load_data
+import oneflow as flow
+import oneflow.typing as oft
+
+BATCH_SIZE = 100
+
+
+def mlp(data):
+    initializer = flow.truncated_normal(0.1)
+    reshape = flow.reshape(data, [data.shape[0], -1])
+    hidden = flow.layers.dense(reshape, 512, activation=flow.nn.relu, kernel_initializer=initializer)
+    return flow.layers.dense(hidden,
+                             10,
+                             kernel_initializer=initializer,
+                             # dense为列存储，进行split(0)切分
+                             model_distribute=flow.distribute.split(axis=0)
+                             )
+
+
+def get_train_config():
+    config = flow.function_config()
+    config.default_data_type(flow.float)
+    config.train.primary_lr(0.1)
+    config.train.model_update_conf({"naive_conf": {}})
+    config.default_distribute_strategy(flow.distribute.consistent_strategy())
+
+    return config
+
+
+@flow.global_function(get_train_config())
+def train_job(images:oft.Numpy.Placeholder((BATCH_SIZE, 1, 28, 28), dtype=flow.float),
+              labels:oft.Numpy.Placeholder((BATCH_SIZE,), dtype=flow.int32)):
+    logits = mlp(images)
+    loss = flow.nn.sparse_softmax_cross_entropy_with_logits(labels, logits, name="softmax_loss")
+    flow.losses.add_loss(loss)
+    return loss
+
+
+if __name__ == '__main__':
+    flow.config.gpu_device_num(2)
+    check_point = flow.train.CheckPoint()
+    check_point.init()
+
+    (train_images, train_labels), (test_images, test_labels) = load_data(BATCH_SIZE)
+
+    for epoch in range(3):
+        for i, (images, labels) in enumerate(zip(train_images, train_labels)):
+            def callback(loss):
+                if i % 20 == 0: print(loss.mean())
+
+
+            loss = train_job(images, labels).async_get(callback)
