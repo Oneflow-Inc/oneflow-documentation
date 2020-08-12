8d15eb7a5afce9aea5a3445abb0aa413d48b64e7
Ziheng ZHAO <68211760+Ziheng-ZHAO@users.noreply.github.com>
2020-08-12 13:24:22 +0800

    Update mixed_parallel_mlp.py

--- cn/docs/code/extended_topics/mixed_parallel_mlp.py
+++ cn/docs/code/extended_topics/mixed_parallel_mlp.py
@@ -1,6 +1,6 @@
 # mixed_parallel_mlp.py
 import oneflow as flow
-import oneflow.typing as tp
+import oneflow.typing as oft
 
 BATCH_SIZE = 100
 
@@ -33,9 +33,9 @@ def get_train_config():
 
 @flow.global_function(type="train", function_config=get_train_config())
 def train_job(
-    images: tp.Numpy.Placeholder((BATCH_SIZE, 1, 28, 28), dtype=flow.float),
-    labels: tp.Numpy.Placeholder((BATCH_SIZE,), dtype=flow.int32),
-) -> tp.Numpy:
+    images: oft.Numpy.Placeholder((BATCH_SIZE, 1, 28, 28), dtype=flow.float),
+    labels: oft.Numpy.Placeholder((BATCH_SIZE,), dtype=flow.int32),
+) -> oft.Numpy:
     logits = mlp(images)
     loss = flow.nn.sparse_softmax_cross_entropy_with_logits(
         labels, logits, name="softmax_loss"
