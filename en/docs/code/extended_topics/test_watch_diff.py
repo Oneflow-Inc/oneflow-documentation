e2103908736dc97a6b0379ca16048f551ecd6f87
Ziheng ZHAO <68211760+Ziheng-ZHAO@users.noreply.github.com>
2020-12-21 15:57:06 +0800

    Update test_watch_diff.py

--- en/docs/code/extended_topics/test_watch_diff.py
+++ en/docs/code/extended_topics/test_watch_diff.py
@@ -2,6 +2,7 @@
 import oneflow as flow
 import oneflow.typing as tp
 
+flow.config.enable_legacy_model_io(False)
 BATCH_SIZE = 100
 
 
@@ -42,9 +43,6 @@ def train_job(
 
 
 if __name__ == "__main__":
-    check_point = flow.train.CheckPoint()
-    check_point.init()
-
     (train_images, train_labels), (test_images, test_labels) = flow.data.load_mnist(
         BATCH_SIZE
     )
