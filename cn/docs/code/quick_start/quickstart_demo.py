566295e9781d0eb3a6892d787767a02b6cab9b36
lychee_l1 <liuqi0108@outlook.com>
2021-07-18 18:40:02 +0800

    Fixed every bug of quickstart_demo.py

--- cn/docs/code/quick_start/quickstart_demo.py
+++ cn/docs/code/quick_start/quickstart_demo.py
@@ -5,38 +5,23 @@ import oneflow as flow
 import torchvision
 import torchvision.transforms as transforms
 
-# # Download training data from open datasets.
-# training_data = datasets.FashionMNIST(
-#     root="data",
-#     train=True,
-#     download=True,
-#     transform=ToTensor(),
-# )
-
-# # Download test data from open datasets.
-# test_data = datasets.FashionMNIST(
-#     root="data",
-#     train=False,
-#     download=True,
-#     transform=ToTensor(),
-# )
-
 # Data manipulation
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 BATCH_SIZE = 100
 (train_images, train_labels), (test_images, test_labels) = flow.data.load_mnist(BATCH_SIZE, BATCH_SIZE)
 
-
 tr_images = torch.tensor(train_images)
 tr_labels = torch.tensor(train_labels)
 te_images = torch.tensor(test_images)
 te_labels = torch.tensor(test_labels)
+
+# 设置模型需要的参数
 input_size = 784
 hidden_size = 100
 num_classes = 10
 batch_size = 100
 
-# Dataloader-MNIST
+# 如果要用DataLoader: Dataloader-MNIST
 # train_dataset = torchvision.datasets.MNIST(root='./data', train=True,
 #     transform=transforms.ToTensor(), download=True)
 # test_dataset = torchvision.datasets.MNIST(root='./data', train=False,
@@ -44,7 +29,6 @@ batch_size = 100
 
 # train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 # test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
-
 
 # Model
 input_size = 784
@@ -55,20 +39,13 @@ batch_size = 100
 class LeNet5(nn.Module):
     def __init__(self, input_size, hidden_size1, hidden_size2, num_classes):
         super(LeNet5, self).__init__()
-        # self.flatten = nn.Flatten()
-        # self.linear_relu_stack = nn.Sequential(
         self.l1 = nn.Linear(input_size, hidden_size1)
         self.relu1 = nn.ReLU()
         self.l2 = nn.Linear(hidden_size1, hidden_size2)
         self.relu2 = nn.ReLU()
         self.l3 = nn.Linear(hidden_size2, num_classes)
-        # snn.ReLU(),
-            # nn.Linear(512, 10),
-            # nn.ReLU()
-        # ) 
 
     def forward(self, x):
-        # x = self.flatten(x)
         out = self.l1(x)
         out = self.relu1(out)
         out = self.l2(out)
@@ -109,6 +86,7 @@ print('Finished Training')
 # torch.save(model.state_dict(), "model.pth")
 # print("Saved PyTorch Model")
 
+# 模型准确率
 with torch.no_grad():
     n_correct = 0
     n_samples = 0
@@ -125,19 +103,13 @@ with torch.no_grad():
     acc = 100.0 * n_correct / n_samples
     print(f'accuracy = {acc}')
 
-# 预测（这块还有bug）
-classes = ('1', '2', '3', '4', '5', '6', '7', '8', '9', '0')
+# 预测
+classes = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
 model.eval()
 te_images = te_images.reshape(-1, 28*28).to(device)
-x, y = te_images[0], te_labels[0]
+x, y = te_images[0], test_labels[0]
 with torch.no_grad():
     pred = model(x)
-    predicted, actual = classes[pred[0].argmax(0)], classes[y]
-    print(f'Predicted:"{predicted}", Actual: "{actucal}"')
-
-
-
-
-
-
-
+    print(pred)
+    predicted, actual = classes[pred.argmax()], y[0]
+    print(f'Predicted:"{predicted}", Actual: "{actual}"')
