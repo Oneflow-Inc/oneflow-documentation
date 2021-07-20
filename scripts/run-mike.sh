8eede2af289e3ef6cd3478b81308c09dddfcf0b8
yaochi <later@usopp.net>
2021-07-20 18:20:33 +0800

    refine multi-lang

--- scripts/run-mike.sh
+++ scripts/run-mike.sh
@@ -1,3 +1,7 @@
 set -ex
 cd cn
-mike deploy 0.5.0.dev --prefix _site -b test_mike --ignore
+mike deploy 0.5.0 latest --prefix _site -b docs_output --ignore
+mike set-default latest --prefix _site -b docs_output
+cd ../en
+mike deploy 0.5.0 latest --prefix _site/en -b docs_output --ignore
+mike set-default latest --prefix _site/en -b docs_output
