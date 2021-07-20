68c529aa0e9b638f9c456906eedf54d72535755c
Yao Chi <yaochi@oneflow.org>
2021-07-20 18:53:55 +0800

    refine run-mike.sh

--- scripts/run-mike.sh
+++ scripts/run-mike.sh
@@ -1,7 +1,7 @@
 set -ex
 cd cn
-mike deploy 0.5.0 latest --prefix _site -b docs_output --ignore
+mike deploy 0.5.0 latest --prefix _site -b docs_output
 mike set-default latest --prefix _site -b docs_output
 cd ../en
-mike deploy 0.5.0 latest --prefix _site/en -b docs_output --ignore
+mike deploy 0.5.0 latest --prefix _site/en -b docs_output
 mike set-default latest --prefix _site/en -b docs_output
