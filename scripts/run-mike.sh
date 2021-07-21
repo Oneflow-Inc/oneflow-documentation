9c3058f8337476a871df17ac397e1fdbf2054228
Yao Chi <later@usopp.net>
2021-07-21 10:48:20 -0500

    versiong v0.4.0 and master (#329)

--- scripts/run-mike.sh
+++ scripts/run-mike.sh
@@ -9,10 +9,21 @@ CN_OPTIONS="--prefix ${CN_SITE} -b ${OUTPUT_BRANCH}"
 EN_OPTIONS="--prefix ${EN_SITE} -b ${OUTPUT_BRANCH}"
 
 cd cn
+
+# master
 ${MIKE} delete --all ${CN_OPTIONS}
+git checkout master
 ${MIKE} deploy master ${LATEST_VERSION} -u ${CN_OPTIONS}
 ${MIKE} set-default ${LATEST_VERSION} ${CN_OPTIONS}
 cd ../en
 ${MIKE} delete --all ${EN_OPTIONS}
 ${MIKE} deploy master ${LATEST_VERSION} -u ${EN_OPTIONS}
 ${MIKE} set-default ${LATEST_VERSION} ${EN_OPTIONS}
+
+# v0.4.0
+VERSION="v0.4.0"
+cd .. && git checkout ${VERSION}
+cd cn
+${MIKE} deploy ${VERSION} -u ${CN_OPTIONS}
+cd ../en
+${MIKE} deploy ${VERSION} -u ${EN_OPTIONS}
