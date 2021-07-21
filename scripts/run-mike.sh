52f8adc2174614d09086babf5e8e4fa0a568f6af
YaoChi <later@usopp.net>
2021-07-21 10:14:21 +0800

    delete all before starting a new deployment

--- scripts/run-mike.sh
+++ scripts/run-mike.sh
@@ -12,8 +12,10 @@ CN_OPTIONS="--prefix ${CN_SITE} -b ${OUTPUT_BRANCH}"
 EN_OPTIONS="--prefix ${EN_SITE} -b ${OUTPUT_BRANCH}"
 
 cd cn
+${MIKE} delete --all ${CN_OPTIONS}
 ${MIKE} deploy master ${LATEST_VERSION} -u ${CN_OPTIONS}
 ${MIKE} set-default ${LATEST_VERSION} ${CN_OPTIONS}
 cd ../en
+${MIKE} delete --all ${EN_OPTIONS}
 ${MIKE} deploy master ${LATEST_VERSION} -u ${EN_OPTIONS}
 ${MIKE} set-default ${LATEST_VERSION} ${EN_OPTIONS}
