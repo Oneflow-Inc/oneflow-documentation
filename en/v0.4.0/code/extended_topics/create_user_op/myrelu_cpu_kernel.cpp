ac1a27c49e6153c7cb754b7ac7bee0807e6fc5b4
doombeaker <doombeaker@users.noreply.github.com>
2021-07-21 15:49:07 +0000

    deploy: 9c3058f8337476a871df17ac397e1fdbf2054228

---
+++ en/v0.4.0/code/extended_topics/create_user_op/myrelu_cpu_kernel.cpp
@@ -0,0 +1,45 @@
+#include "oneflow/core/framework/framework.h"
+
+namespace oneflow {
+
+namespace {
+
+template <typename T>
+void MyRelu(DeviceCtx *ctx, const int64_t n, const T *x, T *y) {
+  T zero = (T)(0);
+  for (int64_t i = 0; i != n; ++i) {
+    y[i] = std::max(x[i], zero);
+  }
+}
+
+template <DeviceType device_type, typename T>
+class ReluKernel final : public user_op::OpKernel {
+public:
+  ReluKernel() = default;
+  ~ReluKernel() = default;
+
+private:
+  void Compute(user_op::KernelComputeContext *ctx) const override {
+    const user_op::Tensor *in_tensor = ctx->Tensor4ArgNameAndIndex("in", 0);
+    user_op::Tensor *out_tensor = ctx->Tensor4ArgNameAndIndex("out", 0);
+    MyRelu<T>(ctx->device_ctx(),
+           in_tensor->shape().elem_cnt(),
+           in_tensor->dptr<T>(), 
+           out_tensor->mut_dptr<T>());
+  }
+  bool AlwaysComputeWhenAllOutputsEmpty() const override { return false; }
+};
+
+#define REGISTER_RELU_KERNEL(device, dtype)          \
+  REGISTER_USER_KERNEL("myrelu")                     \
+      .SetCreateFn<ReluKernel<device, dtype>>()      \
+      .SetIsMatchedHob(                              \
+          (user_op::HobDeviceTag() == device) &     \
+          (user_op::HobDataType("out", 0)            \
+            == GetDataType<dtype>::value));
+
+REGISTER_RELU_KERNEL(DeviceType::kCPU, float)
+REGISTER_RELU_KERNEL(DeviceType::kCPU, double)
+} // namespace
+
+} // namespace oneflow
---
+++ v0.4.0/code/extended_topics/data_loader/mini_reader_op.cpp
@@ -0,0 +1,49 @@
+/*
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#include "oneflow/core/framework/framework.h"
+
+namespace oneflow {
+
+REGISTER_CPU_ONLY_USER_OP("MiniReader")
+    .Output("out")
+    .Attr<std::string>("data_dir")
+    .Attr<int32_t>("data_part_num")
+    .Attr<std::string>("part_name_prefix", "part-")
+    .Attr<int32_t>("part_name_suffix_length", -1)
+    .Attr<int32_t>("batch_size")
+    .Attr<bool>("random_shuffle", false)
+    .Attr<bool>("shuffle_after_epoch", false)
+    .Attr<int64_t>("seed", -1)
+    .Attr<int32_t>("shuffle_buffer_size", 1024)
+    .SetTensorDescInferFn([](user_op::InferContext* ctx) -> Maybe<void> {
+      user_op::TensorDesc* out_tensor = ctx->TensorDesc4ArgNameAndIndex("out", 0);
+      int32_t local_batch_size = ctx->Attr<int32_t>("batch_size");
+      const SbpParallel& sbp = ctx->SbpParallel4ArgNameAndIndex("out", 0);
+      int64_t parallel_num = ctx->parallel_ctx().parallel_num();
+      if (sbp.has_split_parallel() && parallel_num > 1) {
+        CHECK_EQ_OR_RETURN(local_batch_size % parallel_num, 0);
+        local_batch_size /= parallel_num;
+      }
+      *out_tensor->mut_shape() = Shape({local_batch_size, 2});
+      *out_tensor->mut_data_type() = DataType::kDouble;
+      return Maybe<void>::Ok();
+    })
+    .SetGetSbpFn([](user_op::SbpContext* ctx) -> Maybe<void> {
+      ctx->NewBuilder().Split(ctx->outputs(), 0).Build();
+      return Maybe<void>::Ok();
+    });
+
+}  // namespace oneflow
