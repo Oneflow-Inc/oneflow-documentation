f8ccc2b07c6a7c19305782c9904e9e9f6458b5f1
lychee_l1 <liuqi0108@outlook.com>
2021-08-10 10:47:06 +0800

    first edit liuqi

--- cn/docs/code/extended_topics/consistent_strategy.py
+++ cn/docs/code/extended_topics/consistent_strategy.py
@@ -1,6 +1,6 @@
 import numpy as np
-import oneflow as flow
-import oneflow.typing as tp
+from oneflow.compatible import single_client as flow
+from oneflow.compatible.single_client import typing as tp
 
 BATCH_SIZE = 100
 flow.config.enable_legacy_model_io(False)
